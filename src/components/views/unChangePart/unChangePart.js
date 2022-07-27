0a312e8f700d539bdc8de2bad6df5d5b3b59b528
이승일 <seungil@iseung-il.local>
2022-07-27 19:03:34 +0900

    123

--- src/components/views/unChangePart/unChangePart.js
+++ src/components/views/unChangePart/unChangePart.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import style from './App.module.css'
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { useState, useRef } from 'react'
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import style from './mainpage.module.css'
 
-// import LinkStorePage from '';
-function App() {
-  return (
-    <div className="App">
-      <div id={style.wrap}> 
+function unChangePart() {
+
+
+
+
+
+    return (
+        
+        <div id={style.wrap}> 
         <div id={style.header}>
             <div className={style.searching}>
                 <form action="." method="post">
                     <input className={style.search_txt} type="text" placeholder="Search Contents" />
                     <button className={style.button} type="submit">
-                        <img src = "../public/img/ant-design_search-outlined.png" />
+                        <img src = "ant-design_search-outlined.png" />
                     </button>
                 </form>            
             </div>
@@ -27,24 +26,24 @@ function App() {
                 <button className={style.name_button}>James</button>
             </a>
             <a href="#">
-                <img className={style.profile_img} src="../public/img/Ellipse 144.png" />
+                <img className={style.profile_img} src="Ellipse 144.png" />
             </a>
             <a href="#">
-                <img className={style.bell_img} src="../public/img/clarity_bell-outline-badged.png" />
+                <img className={style.bell_img} src="clarity_bell-outline-badged.png" />
             </a>
             <a href="#">
-                <img className={style.setup_img} src="../public/img/clarity_settings-line.png" />
+                <img className={style.setup_img} src="clarity_settings-line.png" />
             </a>
             
         </div>
 
 
         <div id={style.side_left}>
-            <img className = {style.left_line} src="../public/img/Vector 803.png" />
+            <img className = {style.left_line} src="Vector 803.png" />
             <div className = {style.helper_text}>
                 <p>과제헬퍼</p>
             </div>
-            <img className={style.logo_img} src="../public/img/Rectangle 431.png" />
+            <img className={style.logo_img} src="Rectangle 431.png" />
 
     
             <div className={style.menu_wrap}>
@@ -52,7 +51,7 @@ function App() {
                     <input type="checkbox" id={style.ch1} />
                     <label for="ch1" >
                         내 파일
-                        <img className={style.img_ch1} src = "../public/img/ant-design_home-outlined.png" />
+                        <img className={style.img_ch1} src = "ant-design_home-outlined.png" />
                     </label>
                         
                     <ul className={style.sub_menu}>
@@ -64,13 +63,13 @@ function App() {
                 <a href="#">
                     <div className={style.ch2}>
                         <p>즐겨찾기</p>
-                        <img className={style.img_ch2} src = "../public/img/clarity_star-line.png" />
+                        <img className={style.img_ch2} src = "clarity_star-line.png" />
                     </div>
                 </a>
                 <a href="#">
                     <div className={style.ch3}>
                         <p>휴지통</p>
-                        <img className={style.img_ch3} src = "../public/img/ion_trash-outline.png" />
+                        <img className={style.img_ch3} src = "ion_trash-outline.png" />
                     </div>    
                 </a>
                 
@@ -89,21 +88,13 @@ function App() {
 
 
 
-        <div id={style.contents}>
-          {/* <Router>
-            <div>
-              <Routes>
-                <Route exact path="/linkstore" element={ < LinkStorePage /> } />
-              </Routes>
-            </div>
-          </Router> */}
-        </div>
+        <div id={style.contents}>contents</div>
 
 
 
         <div id={style.side_right}>
             <div className = {style.team_make}>
-                <img className = {style.team_img} src="../public/img/ant-design_team-outlined.png" />
+                <img className = {style.team_img} src="ant-design_team-outlined.png" />
                 <div className = {style.teamfolder_text}>
                     <p>팀 폴더</p>
                 </div>
@@ -112,16 +103,16 @@ function App() {
             <div className = {style.teamfolder_menu}>
                 <div className={style.team_profile}>
                     <a href="#">
-                        <img className={style.img_yellow} src="../public/img/Ellipse 159.png" />
+                        <img className={style.img_yellow} src="Ellipse 159.png" />
                     </a>
                     <a href="#">
                         <input type="button" className={style.team_text} value="과제헬퍼"/>
                     </a>
                     <a href="#">
-                        <img className={style.img_teamperson} src="../public/img/bi_person-plus.png" />
+                        <img className={style.img_teamperson} src="bi_person-plus.png" />
                     </a>
                     <a href="#">
-                        <img className={style.img_folder} src="../public/img/fluent_folder-open-24-regular.png" />
+                        <img className={style.img_folder} src="fluent_folder-open-24-regular.png" />
                     </a>
                 </div>
                 <div></div>
@@ -144,12 +135,12 @@ function App() {
 
         <div id={style.side_right2}>
             <a href="#">
-                <img className={style.img_pay} src="../public/img/Rectangle 440.png" />
+                <img className={style.img_pay} src="Rectangle 440.png" />
             </a>
             <div className={style.pay_text}>
                 <p>개인 프로 요금제로 업그레이드 하기</p>
             </div>
-            <img className = {style.pay_person} src="../public/img/undraw_update_re_swkp (1) 2.png" />
+            <img className = {style.pay_person} src="undraw_update_re_swkp (1) 2.png" />
             <div className={style.pay_text2}>
                 <p>현재 파일 당 최대 크기는</p>
                 <p>0MB입니다.</p>
@@ -160,13 +151,11 @@ function App() {
 
         </div>
         
-      </div>
     </div>
-  );
+    )
+
+
 }
 
-export default App;
 
-
-
-
+export default unChangePart;
